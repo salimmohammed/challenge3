c98aa3f3cd3e90d1eecf39309f3cf8b026faa157
Salim <salim.mohammed@addepar.com>
2016-05-02 19:35:58 -0700

    Added files via upload
    Challenge 3 - Salim

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/*
-  HELPER FUNCTIONS
-*/
-
 function each(array, f) {
   for (var i = 0; i < array.length; i++) {
     f(array[i]);
@@ -34,6 +30,66 @@ function map(array, f) {
   return acc;
 }
 
-/*
-  YOUR CODE GOES BELOW HERE
-*/
+//1. The following function wordLengths accepts a string as a parameter and returns an array of the lengths of each word in the string.
+//Rewrite wordLengths using each (written above for your convenience); that is, replace the for loop.
+//wordLengths("The quick brown fox jumps over the lazy dog.")
+// => [ 3, 5, 5, 3, 5, 4, 3, 4, 4 ]
+
+function wordLengths(string) {
+  var acc = [];
+  var words = string.split(" ");
+  for (var i = 0; i < words.length; i++) {
+    acc.push(words[i].length);
+  }
+  return acc;
+}
+
+
+var wordLengths2 = function (string) {
+	var acc = [];
+	var words = string.split(" ");
+	each (words, function(words) {
+		acc.push(words.length);
+	});
+	return acc;
+}
+
+
+// 2. wordLengths involves transforming every word into its length, which makes this function an ideal
+// use-case of the map abstraction. Using map (also written above for convenience), rewrite wordLengths
+// (but write a new function, e.g. wordLengthsMap) again -- but this time using map.
+
+
+
+
+
+
+
+//3. Write a function called wordsLongerThanThree using filter that, given a string as input,
+//returns an array of the words in the original string that are longer than three (have length greater than three).
+
+function wordsLongerThanThree (string) {
+  	var words = string.split(" ");
+	return filter (words, function (str) {
+		return str.length > 3;
+	})
+}
+
+
+
+// 4. Write a function called concat that takes two arrays as parameters
+// and returns an array with all of the elements of the second one added to the first one
+// -- your implementation should use reduce. It should work like this:
+
+
+function concat (arr1, arr2) {
+	return reduce (concat, function (total) {
+	  return total + "," + arr2;
+	}, arr1);
+}
+
+concat([1, 2, 3], [4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+concat([], [3, 4, 5]); // => [3, 4, 5]
+concat([], []); // => []
+
+
